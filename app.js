f24220ecb79ec2d40f000389f271a811db867f1c
SammyHam <32743644+SammyHam@users.noreply.github.com>
2017-10-29 18:19:43 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+{
+  "name": "Dialogflow-webhook",
+  "description": "Dialogflow-webhook",
+  "repository": "https://github.com/SammyHam/Dialogflow-webhook",
+  "keywords": ["api.ai", "natural language", "dialogflow", "chatbot", "webhook"]
+}
