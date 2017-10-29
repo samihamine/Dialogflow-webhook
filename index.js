da08fe2dfaabf09192d9158f95a0aa4a6e6abc2c
SammyHam <32743644+SammyHam@users.noreply.github.com>
2017-10-29 18:14:30 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const restService = express();
+
+restService.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+restService.use(bodyParser.json());
+
+restService.post('/webhook', function(req, res) {
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Oups quleques problèmes de connexion, peux-tu répéter s'il te plaît ?"
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: 'webhook-echo-sample'
+    });
+});
+
+restService.listen((process.env.PORT || 8000), function() {
+    console.log("Server up and listening");
+});
